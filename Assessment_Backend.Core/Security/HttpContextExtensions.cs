3a2fed839f386d336c3339d9b93093ff48af6a12
rezakazazy8 <rezakazazy8@yahoo.com>
2024-04-21 21:57:42 +0330

    fixedtext for  trim email

---
+++ Assessment_Backend.Core/Security/HttpContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+public static class HttpContextExtensions
+{
+    public static int GetUserId(this IHttpContextAccessor httpContextAccessor)
+    {
+        try
+        {
+            int userId;
+            //if (!int.TryParse(httpContextAccessor.HttpContext.User.FindFirstValue("userId"), out userId))
+            //{
+            //    //_logger.LogError("Failed to parse userId. User is not authenticated or userId is missing.");
+            //    return 0;
+            //}
+            return 1;
+        }
+        catch (Exception)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Assessment_Backend.Core/Security/ValidateModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Assessment_Backend.Core.Security
+{
+    public class ValidateModel
+    {
+        public static bool Validate<T>(T model, out string validationResult)
+        {
+            validationResult = "";
+
+            var validationContext = new ValidationContext(model, serviceProvider: null, items: null);
+            var validationResults = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(model, validationContext, validationResults, true))
+            {
+                foreach (var validationResultItem in validationResults)
+                {
+                    validationResult += validationResultItem.ErrorMessage + " ";
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
