b6935d931e6d60e5e0f38006066f775f751bbc01
rezakazazy8 <rezakazazy8@yahoo.com>
2024-04-22 15:29:31 +0330

    Create DTO OutOutModel

---
+++ Assessment_Backend.Core/DTOs/OutPutModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Assessment_Backend.Core.DTOs
+{
+    public class OutPutModel<T>
+    {
+        public T? Result { get; set; }
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+    }
+}
