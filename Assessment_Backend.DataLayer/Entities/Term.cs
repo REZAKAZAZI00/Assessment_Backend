ef6169efe8db347a735331c8cf205d50c24a409b
rezakazazy8 <rezakazazy8@yahoo.com>
2024-04-26 18:27:14 +0330

    Create Table term

---
+++ Assessment_Backend.DataLayer/Entities/Term.cs
@@ -0,0 +1,19 @@
+﻿namespace Assessment_Backend.DataLayer.Entities
+{
+    public class Term
+    {
+        [Key]
+        public int TermId { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsDelete { get; set; }
+
+
+        #region Relations
+
+        public Course Course { get; set; }
+
+        #endregion
+    }
+}
