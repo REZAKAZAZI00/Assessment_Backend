ed5a9a380f96b00d4741549ff59f3581a758ccbf
rezakazazy8 <rezakazazy8@yahoo.com>
2024-05-05 23:38:00 +0330

    carete dto for login

---
+++ Assessment_Backend.Core/DTOs/Account/Login.cs
@@ -0,0 +1,18 @@
+﻿namespace Assessment_Backend.Core.DTOs.Account
+{
+    public class LoginDTO
+    {
+
+        [Display(Name = " کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string Password { get; set; }
+
+        [Display(Name = " کد ملی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string CodeMelli { get; set; }
+    }
+}
