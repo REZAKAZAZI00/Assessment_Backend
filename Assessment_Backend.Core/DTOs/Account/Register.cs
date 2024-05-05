f597c98d9ba209d4dae29a3621d504db0627d198
rezakazazy8 <rezakazazy8@yahoo.com>
2024-05-05 23:38:48 +0330

    create dto for register

---
+++ Assessment_Backend.Core/DTOs/Account/Register.cs
@@ -0,0 +1,90 @@
+﻿namespace Assessment_Backend.Core.DTOs.Account
+{
+    public class RegisterTeacherDTO
+    {
+
+        [Display(Name = " نام  ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(70, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public string Name { get; set; }
+
+        [Display(Name = " نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(70, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public string family { get; set; }
+
+        [Display(Name = "شماره تلفن همراه ")]
+        [MaxLength(11, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "ایمیل ")]
+        [MaxLength(150, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+
+        [Display(Name = " کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string Password { get; set; }
+
+        [Display(Name = " کد ملی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string CodeMelli { get; set; }
+
+        [Display(Name = "کد استادی ")]
+        [MaxLength(15, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string TeacherCode { get; set; }
+
+        public int RoleId { get; set; }
+
+    }
+
+    public class RegisterStudentDTO
+    {
+
+        public int GradeId { get; set; }
+
+        [Display(Name = " نام  ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(70, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public string Name { get; set; }
+
+        [Display(Name = " نام خانوادگی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(70, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public string family { get; set; }
+
+        [Display(Name = "شماره تلفن همراه ")]
+        [MaxLength(11, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "ایمیل ")]
+        [MaxLength(150, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+
+        [Display(Name = " کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string Password { get; set; }
+
+        [Display(Name = " کد ملی")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(10, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+
+        public required string CodeMelli { get; set; }
+
+
+        public int RoleId { get; set; }
+    }
+}
