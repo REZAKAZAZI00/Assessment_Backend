098b04b06a7f9dd750459f33ea566a66612cd853
rezakazazy8 <rezakazazy8@yahoo.com>
2024-05-05 23:39:10 +0330

    create dto for role

---
+++ Assessment_Backend.Core/DTOs/Role/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace Assessment_Backend.Core.DTOs.Role
+{
+    public class RoleDTO
+    {
+        public int RoleId { get; set; }
+
+        [Display(Name = "عنوان  ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [MaxLength(50, ErrorMessage = "{0} نمی تواند بیشتر از {1} کاراکتر باشد .")]
+        public required string Title { get; set; }
+    }
+}
