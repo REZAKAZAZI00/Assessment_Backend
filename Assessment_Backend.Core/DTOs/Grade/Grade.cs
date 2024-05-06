a369f57f51f8b9d984dd5646faec6d783c369052
rezakazazy8 <rezakazazy8@yahoo.com>
2024-05-06 14:29:54 +0330

    create Grade dto

---
+++ Assessment_Backend.Core/DTOs/Grade/Grade.cs
@@ -0,0 +1,10 @@
+﻿namespace Assessment_Backend.Core.DTOs.Grade
+{
+    public class GradeDTO
+    {
+        public int GradeId { get; set; }
+
+        [Display(Name = "عنوان  ")]
+        public string Title { get; set; }
+    }
+}
