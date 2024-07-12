0927e1b51d95f9a84421750e3943c830a4a1c955
rezakazazy8 <rezakazazy8@yahoo.com>
2024-07-12 15:55:48 +0330

    add config Migrate database

--- Assessment_Backend/Program.cs
+++ Assessment_Backend/Program.cs
@@ -39,6 +39,14 @@ internal class Program
         var connectionString = builder.Configuration.GetConnectionString("AssessmentConnection");
         builder.Services.AddDbContext<AssessmentDbContext>(options => options.UseSqlServer(connectionString));
         #endregion
+        var options=new DbContextOptionsBuilder<AssessmentDbContext>()
+            .UseSqlServer(connectionString).Options;
+        using AssessmentDbContext context=new AssessmentDbContext(options);
+        b
+        if (context.Database.GetPendingMigrations().Count()>0)
+        {
+            context.Database.Migrate();
+        }
 
         #region IOC
 
